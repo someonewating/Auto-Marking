dd3bcd519a848eb8becd6a0420cfa6e7c6ccfab3
someonewating <1870828369@qq.com>
2021-01-27 18:09:35 +0800

    将执行过程分离，写成函数体。

--- Mark.py
+++ Mark.py
@@ -4,15 +4,20 @@ import shutil
 import io
 import sys
 
-def copy():
-   path = r'e:\\Homework\\Auto-Book'
-   path_list = os.listdir(path)
-   path_list.remove('.git')    # macos中的文件管理文件，默认隐藏，这里可以忽略
-   path_list.remove('.gitattributes')
+path = input('输入目标文件夹：')
+path_list = os.listdir(path)
+
+def num(path):
+   path_list.remove('.git')
    path_list.sort()
-   #print(path_list)
+
+   return path,path_list
+
+def copy(path,path_list):
+   num(path)
    target = r'e:\\Homework\\Auto-Copy\\test'
    for filename in path_list:
       print(os.path.join(path,filename))
       shutil.copy(os.path.join(path, filename), target)
-copy()
+
+copy(path,path_list)
