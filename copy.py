1866dce73c4d120c69b11addf3eb06f13d985ca7
someonewating <1870828369@qq.com>
2021-01-28 22:12:14 +0800

    从源文件夹复制到临时文件夹，后从临时文件夹复制到目标文件夹。——成功

--- copy.py
+++ copy.py
@@ -6,17 +6,16 @@ from time import sleep
 
 #path = input('输入源文件夹路径：')
 path = r'E:\\KSWJJ\\Answers'
-path_list = os.listdir(path)
-temp_target = r'e:\\Homework\\Auto-Copy\\test\\temp'
-temp_target_list = os.listdir(temp_target)
+temp_target = r'e:\Homework\Auto-Copy\test\temp'
 target = r'e:\Homework\Auto-Copy\test\result'
-
+'''
 def num():
    path_list.sort()        #按顺序排列文件夹
    return path,path_list
-
-def copy2temp(path,path_list,temp_target):
+'''
+def copy2temp(path,temp_target):
    #num()
+   path_list = os.listdir(path)
    path_list.sort()
    for filename in path_list:
       if os.path.exists(temp_target):
@@ -26,13 +25,14 @@ def copy2temp(path,path_list,temp_target):
       print (filename + '复制就位！')
       sleep(5)
       copy2result(temp_target,target)
-      print ('复制到result成功')
+      print (filename + '复制到result成功')
       
 def copy2result(temp_target,target): 
    #num()
+   temp_target_list = os.listdir(temp_target)
    temp_target_list.sort()
    for filename in temp_target_list:
       shutil.copy(os.path.join(temp_target, filename), target)
 
-copy2temp(path,path_list,temp_target)
+copy2temp(path,temp_target)
 
